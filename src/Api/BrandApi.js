913a61c7e7ad11f65b04809713fb96521496b17d
SayaliChavan2003 <sayali@codxsoftwares.com>
2026-04-23 19:10:55 +0530

    brandApi

--- src/Api/BrandApi.js
+++ src/Api/BrandApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const brandApi = createApi({
   reducerPath: "brandApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://api.thecodx.com/api",
   }),
   endpoints: (builder) => ({
 
